bfe5d172c0dadbf43a7d2191fda12249e9c977d9
Matthias Müller-Brockhausen <git@mmb2.click>
2018-12-08 13:10:42 +0100

    :bug: fix status not being reset on error

--- packages/xpfw-ui-shared/src/store/list.ts
+++ packages/xpfw-ui-shared/src/store/list.ts
@@ -86,9 +86,10 @@ export class ListStore extends StoreBase implements IPersistableStore {
 
   public async makeQuery(form: IForm, prefix: string = "") {
     const getAt = `${prefix && prefix.length > 0 ? prefix + "." : ""}${form.model}`
+    let qKey: any
     try {
       const queryObj = this.buildQueryObj(form, prefix)
-      const qKey = `${JSON.stringify(form.multiCollection)}${form.collection}${JSON.stringify(queryObj)}`
+      qKey = `${JSON.stringify(form.multiCollection)}${form.collection}${JSON.stringify(queryObj)}`
       if (!this.doingQuery[qKey]) {
         this.doingQuery[qKey] = true
         LoadingStore.setLoading(getAt, true)
@@ -140,6 +141,7 @@ export class ListStore extends StoreBase implements IPersistableStore {
     } catch (error) {
       this.lists[getAt] = {error}
       this.trigger(getAt)
+      this.doingQuery[qKey] = false
       return {error}
     }
   }
