cafebf486d73b67d07e22c245506856dc326b6e1
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-10 09:04:20 +0100

    :bug: prevent queries for "undefined"

--- packages/xpfw-validate/src/fieldConverter/textRegex.ts
+++ packages/xpfw-validate/src/fieldConverter/textRegex.ts
@@ -1,6 +1,9 @@
 import { FieldConverter } from "../typeDef"
 
 const convertTextToMongoRegex: FieldConverter = (value) => {
+  if (value == null || value.length === 0) {
+    return null
+  }
     return {
         $regex: `(.*?)${value}(.*?)`,
         $options: "isg"
