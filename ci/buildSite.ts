c3a0b449bc24bee61c9fa6458cbbecf78a04b996
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-10 09:00:51 +0100

    :wrench: build fixes

--- ci/buildSite.ts
+++ ci/buildSite.ts
@@ -3,8 +3,6 @@ import { resolve } from "path"
 
 rm("-rf", resolve(`site/webpackDist/`))
 rm("-rf", resolve(`docs/_book`))
-exec("npm run build", {cwd: resolve(`site`)})
-exec("npm run buildPre", {cwd: resolve(`site`)})
-exec("npm run preRender", {cwd: resolve(`site`)})
+exec("npm run static", {cwd: resolve(`site`)})
 exec("gitbook build", {cwd: resolve(`docs`)})
 cp("-r", resolve(`docs/_book/`), resolve(`site/webpackDist/docs/`))
--- site/webpack.config.pre.ts
+++ site/webpack.config.pre.ts
@@ -14,7 +14,7 @@ const nodeModules: any = {}
 
 const webpackServerConfig: webpack.Configuration = {
   target: "node" as "node",
-  mode: "production",
+  mode: "development",
   externals: [nodeModules],
   entry: "./src/prerender.tsx",
   output: {
