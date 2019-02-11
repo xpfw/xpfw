8d073c34494b212b627e8a4085fec751ae116023
Matthias Müller-Brockhausen <git@mmb2.click>
2019-02-11 15:55:30 +0100

    :page_facing_up: Hook for field with validation

---
+++ packages/form/src/components/field.test.ts
@@ -0,0 +1,49 @@
+import { JSONSchemaDefinition } from "../jsonschema"
+import FormStore from "../store/form"
+import { useField, useFieldWithValidation} from "./field"
+
+const stringScheme: JSONSchemaDefinition = {
+  type: "string"
+}
+const numberScheme: JSONSchemaDefinition = {
+  type: "number"
+}
+
+describe("Form Hooks Test", () => {
+  it("useField", () => {
+    const valueToSet = "myStringValue"
+    const valueOne = "theFirstValue"
+    const valueTwo = "mySecondVal"
+    FormStore.setValue(valueToSet,  valueOne)
+    let stringHook = useField(valueToSet, stringScheme)
+    expect(stringHook.value).toBe(valueOne)
+    stringHook.setValue(valueTwo)
+    stringHook = useField(valueToSet, stringScheme)
+    expect(stringHook.value).toBe(valueTwo)
+  })
+  it("useFieldWithValidation", () => {
+    const valueToSet = "myNumberValue"
+    const valueOne = "theFirstValue"
+    const valueTwo = 42
+    // set valid value
+    let numberHook = useFieldWithValidation(valueToSet, numberScheme)
+    numberHook.setValue(valueTwo)
+    numberHook = useFieldWithValidation(valueToSet, numberScheme)
+    expect(numberHook.value).toBe(valueTwo)
+    expect(numberHook.error).toMatchSnapshot(" no error because valid number ")
+    // set invalid value
+    numberHook.setValue(valueOne)
+    numberHook = useFieldWithValidation(valueToSet, numberScheme)
+    expect(numberHook.value).toBe(valueOne)
+    const initialError = numberHook.error
+    expect(numberHook.error).toMatchSnapshot("Error because not a valid number instead it's a string")
+    // set valid value with prefix while previous value should stay untouched due to the prefix in use
+    let withPrefix = useFieldWithValidation(valueToSet, numberScheme, "myPrefixValue")
+    withPrefix.setValue(valueTwo)
+    numberHook = useFieldWithValidation(valueToSet, numberScheme)
+    expect(numberHook.value).toBe(valueOne)
+    expect(initialError).toBe(numberHook.error)
+    withPrefix = useFieldWithValidation(valueToSet, numberScheme, "myPrefixValue")
+    expect(withPrefix.value).toBe(valueTwo)
+  })
+})
--- packages/form/src/components/field.ts
+++ packages/form/src/components/field.ts
@@ -1,4 +1,6 @@
+import { action } from "mobx"
 import { JSONSchemaDefinition } from "../jsonschema"
+import jsonValidator from "../jsonValidator"
 import FormStore from "../store/form"
 
 const useField = (name: string, definition: JSONSchemaDefinition, prefix?: string) => {
@@ -11,6 +13,21 @@ const useField = (name: string, definition: JSONSchemaDefinition, prefix?: strin
   }
 }
 
+const useFieldWithValidation = (name: string, definition: JSONSchemaDefinition, prefix?: string) => {
+  const originalWrapped = useField(name, definition, prefix)
+  const setValue = action ((newValue: any) => {
+    originalWrapped.setValue(newValue)
+    const validationResults = jsonValidator.validate(definition, newValue)
+    if (validationResults === false) {
+      FormStore.setError(name, jsonValidator.errors, prefix)
+    }
+  })
+  return {
+    value: originalWrapped.value, setValue,
+    error: FormStore.getError(name, prefix)
+  }
+}
+
 export {
-  useField
+  useField, useFieldWithValidation
 }
