706ce77cbfe1e52254a9f5a124fffc1ede02dd97
Matthias Müller-Brockhausen <git@mmb2.click>
2019-02-12 10:49:39 +0100

    :recycle: Memoize prefix functions

--- packages/form/src/util/prefixMaker.ts
+++ packages/form/src/util/prefixMaker.ts
@@ -1,10 +1,24 @@
+import { memoize } from "lodash-es"
+
 /**
  * prefixMaker ensures that a string ends in a dot
  * This is done so that prefixed values are saved in an own object inside of e.g. FormStore
  * @param prefix
  */
-const prefixMaker = (prefix?: string) => {
+const prefixMaker = memoize((prefix?: string) => {
   return prefix && prefix.length > 0 ? prefix + "." : ""
-}
+})
+
+/**
+ * Prepend a prefix if provided
+ * This is done so that prefixed values are saved in an own object inside of e.g. FormStore
+ * @param prefix
+ */
+const prependPrefix = memoize((mapTo: string, prefix?: string) => {
+  return `${prefixMaker(prefix)}${mapTo}`
+})
 
 export default prefixMaker
+export {
+  prependPrefix
+}
