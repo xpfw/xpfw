c91fcddff164150af34adbce98613c97474dfb14
Matthias Müller-Brockhausen <git@mmb2.click>
2019-02-13 16:12:27 +0100

    :recycle: Add exports

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,7 +1,10 @@
+import useArray from "./hooks/array"
 import { useField, useFieldWithValidation } from "./hooks/field"
+import useObject from "./hooks/object"
 import FormStore, { FormStoreClass } from "./store/form"
 import prefixMaker from "./util/prefixMaker"
 
 export {
-  useField, useFieldWithValidation, prefixMaker, FormStore, FormStoreClass
+  useField, useFieldWithValidation, prefixMaker, FormStore, FormStoreClass,
+  useArray, useObject
 }
