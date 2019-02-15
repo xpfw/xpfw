0da0c0ae0f67fb668ce755e14db30e4abe737535
Matthias Müller-Brockhausen <git@mmb2.click>
2019-02-15 14:46:13 +0100

    :recycle: also export prependPrefix

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -7,10 +7,10 @@ import ComponentRegistry, { IFieldProps } from "./store/componentRegistry"
 import FormStore, { FormStoreClass } from "./store/form"
 import getMapToFromProps from "./util/getMapToFromProps"
 import memo from "./util/memo"
-import prefixMaker from "./util/prefixMaker"
+import prefixMaker, { prependPrefix } from "./util/prefixMaker"
 
 export {
   useField, useFieldWithValidation, prefixMaker, FormStore, FormStoreClass,
   useArray, useObject, ComponentRegistry, JSONSchemaDefinition, IFieldProps,
-  SharedField, getMapToFromProps, memo, ExtendedJSONSchema
+  SharedField, getMapToFromProps, memo, ExtendedJSONSchema, prependPrefix
 }
