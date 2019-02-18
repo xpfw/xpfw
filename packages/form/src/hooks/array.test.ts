fb760c0214bd16f13b2ede24cb987082c6f660d6
Matthias Müller-Brockhausen <git@mmb2.click>
2019-02-18 16:01:52 +0100

    :bug: fix array test

--- packages/form/src/hooks/array.test.ts
+++ packages/form/src/hooks/array.test.ts
@@ -2,17 +2,17 @@ import { JSONSchemaDefinition } from "../jsonschema"
 import FormStore from "../store/form"
 import useArrayHelper from "./array"
 
+const def: JSONSchemaDefinition = {title: "myStringValue", items: {type: "string"}}
 describe("Form Hooks Test", () => {
   it("useArrayHelper", () => {
-    const valueToSet = "myStringValue"
-    let arrayHelper = useArrayHelper(valueToSet)
+    let arrayHelper = useArrayHelper(def)
     expect(FormStore.formData).toMatchSnapshot("Before anything")
     arrayHelper.fields[0].increaseSize()
     expect(FormStore.formData).toMatchSnapshot("After calling increase size on subfield")
     arrayHelper.increaseSize()
     arrayHelper.increaseSize()
     expect(FormStore.formData).toMatchSnapshot("After calling increase size on root")
-    arrayHelper = useArrayHelper(valueToSet)
+    arrayHelper = useArrayHelper(def)
     FormStore.setValue(arrayHelper.fields[1].mapTo, " set specific value ")
     expect(FormStore.formData).toMatchSnapshot("After Using subfield map to with set value")
     arrayHelper.fields[1].decreaseSize()
