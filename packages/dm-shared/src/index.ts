2bd988b8f6ac8571a756a71e45dae12d2b38685c
Matthias Müller-Brockhausen <git@mmb2.click>
2019-03-30 13:12:53 +0100

    :bug: fix index exports

--- packages/dm-shared/src/index.ts
+++ packages/dm-shared/src/index.ts
@@ -1,8 +1,9 @@
 import { TimeBeginField, TimeEndField } from "./components/fields"
-import SharedStatWrapper from "./components/stat"
+import useStat from "./components/stat"
 import StatStore from "./store/stat"
 
 export {
-  StatStore, SharedStatWrapper,
-  TimeBeginField, TimeEndField
+  StatStore,
+  TimeBeginField, TimeEndField,
+  useStat
 }
