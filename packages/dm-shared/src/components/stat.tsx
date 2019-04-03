6a88b463311d97646e2da83b97fec99bf547e5d1
Matthias Müller-Brockhausen <git@mmb2.click>
2019-04-03 13:35:10 +0200

    :bug: Fix date queries

--- packages/dm-shared/src/components/stat.tsx
+++ packages/dm-shared/src/components/stat.tsx
@@ -1,20 +1,34 @@
 import { ensureDate, IStatConfig, StatType } from "@xpfw/dm"
 import { FormStore, prependPrefix } from "@xpfw/form"
+import { set } from "lodash"
 import StatStore from "../store/stat"
 import { TimeBeginField, TimeEndField } from "./fields"
 
-const useStat = (config: IStatConfig, collection: string, useServer?: boolean, prefix?: string) => {
+export interface IUseStatOptions {
+  /**
+   * Whether to use calculate the data locally on the client side via many find calls or whether this should be taken care of by the server
+   */
+  useServer?: boolean
+  /**
+   * If this is specified $gte and $lte will be set to the values of `TimeBeginField` and `TimeEndField`
+   */
+  dateQueryPath?: string
+}
+
+const useStat = (config: IStatConfig, collection: string, options?: IUseStatOptions,  prefix?: string) => {
   const query: any = {}
-  if (config.type === StatType.timeStep) {
+  const useServer = options != null ? options.useServer : false
+  if (options != null && options.dateQueryPath != null) {
     const gte: any = FormStore.getValue(TimeBeginField.title, prefix)
+    if (gte != null) {
+      set(query, `${options.dateQueryPath}.$gte`, ensureDate(gte))
+    }
     const lte: any = FormStore.getValue(TimeEndField.title, prefix)
-    query.createdAt = {
-      $gte: ensureDate(gte),
-      $lte: ensureDate(lte)
+    if (lte != null) {
+      set(query, `${options.dateQueryPath}.$lte`, ensureDate(lte))
     }
   }
-  const stat: any = StatStore.getStat(
-    `${prependPrefix(collection, prefix)}.${config.id}`,
+  const stat: any = StatStore.getStat(`${prependPrefix(collection, prefix)}.${config.id}`,
     collection, config, query, useServer)
   return {
     stat, config, query
