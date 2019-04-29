34ed6eb998ba3bd0c52f6350c5e9e1bacf29ef9d
Matthias Müller-Brockhausen <git@mmb2.click>
2019-04-29 17:09:33 +0200

    :bug: Fix Order related issue

--- ci/build.ts
+++ ci/build.ts
@@ -1,10 +1,10 @@
 import { readdirSync } from "fs"
 import { exec } from "shelljs"
-import { resolve } from "path";
+import { resolve } from "path"
+const packages = ["test-util","form","permission","router","dm","data","form-web","form-bulma","form-tests","feathers-hooks","data-nedb","data-feathers","data-tests","data-bulma","dm-shared"]
 const makeUpdates = async function() {
-  const subPackages = readdirSync("packages")
-  console.log("Building " + subPackages.length + " Packages")
-  for (const subPackage of subPackages) {
+  console.log("Building " + packages.length + " Packages")
+  for (const subPackage of packages) {
     console.log(`Building ${subPackage}`)
     exec(`npm run build`, {cwd: resolve(`packages/${subPackage}`)})
   }
