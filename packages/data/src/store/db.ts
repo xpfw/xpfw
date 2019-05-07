9cb2027732c69dcaf235fb46eadeb0fa0d8827a0
Matthias Müller-Brockhausen <git@mmb2.click>
2019-05-07 16:38:18 +0200

    :bug: Fix Get not alwaysupdating due to action

--- packages/data/src/store/db.ts
+++ packages/data/src/store/db.ts
@@ -17,36 +17,6 @@ export interface FormToUpdate {
 }
 
 export class DbStoreClass {
-
-  public getFromServer = flow(function *(this: DbStoreClass, id: string, collection: string) {
-    if (this.getState[collection] && this.getState[collection][id]) {
-      if (this.lastFetch[id] && Date.now() - this.lastFetch[id] < FETCH_THRESHOLD) {
-        return this.getState[collection][id]
-      }
-    }
-    if (this.fetching[id] === true || Date.now() - this.lastFetch[id] < FETCH_THRESHOLD) {
-      return
-    }
-    if (this.getState[collection] == null) {
-      this.getState[collection] = {}
-    }
-    try {
-      this.fetching[id] = true
-      FormStore.setLoading(id, true)
-      this.lastFetch[id] = Date.now()
-      const result = yield BackendClient.client.get(collection, id)
-      this.getState[collection][id] = result
-      FormStore.setError(id, undefined)
-      FormStore.setLoading(id, false)
-      this.fetching[id] = false
-      return result
-    } catch (error) {
-      FormStore.setLoading(id, false)
-      FormStore.setError(id, error)
-      this.fetching[id] = false
-      return error
-    }
-  })
   @observable
   public currentlyEditing: string = ""
 
@@ -64,7 +34,39 @@ export class DbStoreClass {
   @observable
   private lastFetch: {[index: string]: number} = {}
   @observable
-  private fetching: {[index: string]: boolean} = {}
+  private fetching: {[index: string]: any} = {}
+
+  public async getFromServer(id: string, collection: string) {
+    if (this.getState[collection] && this.getState[collection][id]) {
+      if (this.lastFetch[id] && Date.now() - this.lastFetch[id] < FETCH_THRESHOLD) {
+        return Promise.resolve(this.getState[collection][id])
+      }
+    }
+    if (this.fetching[id] == null) {
+      const thisRef = this
+      this.fetching[id] = flow(function *(this: DbStoreClass) {
+        if (thisRef.getState[collection] == null) {
+          thisRef.getState[collection] = {}
+        }
+        try {
+          FormStore.setLoading(id, true)
+          thisRef.lastFetch[id] = Date.now()
+          const result = yield BackendClient.client.get(collection, id)
+          thisRef.getState[collection][id] = result
+          FormStore.setError(id, undefined)
+          FormStore.setLoading(id, false)
+          delete thisRef.fetching[id]
+          return result
+        } catch (error) {
+          FormStore.setLoading(id, false)
+          FormStore.setError(id, error)
+          delete thisRef.fetching[id]
+          return error
+        }
+      })()
+    }
+    return this.fetching[id]
+  }
 
   public getEditOriginal(id: string, schema: ExtendedJSONSchema,
                          mapTo?: string, prefix: string = "", returnFetchPromise?: boolean) {
@@ -80,7 +82,7 @@ export class DbStoreClass {
         this.currentlyEditing = id
         mapTo = getMapTo(schema, mapTo)
         if (result == null) {
-          const fetchPromise = this.getFromServer(id, collection).then(action((res) => {
+          const fetchPromise = this.getFromServer(id, collection).then(action((res: any) => {
             const doc = this.getState[collection][id]
             this.updateState[saveResultAt] = null
             FormStore.setValue(mapTo, toJS(doc), prefix)
@@ -153,7 +155,6 @@ export class DbStoreClass {
     }
   }
 
-  @action
   public getGetState(id: string, collection: string, tryFetch: boolean) {
     if (tryFetch) {
       setTimeout(() => this.getFromServer(id, collection), 1)
